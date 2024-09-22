0c8d9ea44db620e3fecdde7a99e4937d32ca1f19
shourov630 <shourovhassan6305@gmail.com>
2024-09-23 01:39:10 +0600

    hmm

--- js/dom.js
+++ js/dom.js
@@ -3,4 +3,5 @@ const donate = document.getElementById('btn-don');
 const history = document.getElementById('btn-his');
 let readyMoney = document.getElementById('ready-money');
 const inputAmount = document.getElementById('input-amount');
+console.log(readyMoney.innerText);
 
