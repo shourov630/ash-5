920580cae41385cff00ce1714ff7973089a9dbd1
shourov630 <shourovhassan6305@gmail.com>
2024-09-25 03:52:16 +0600

    all

--- js/dom.js
+++ js/dom.js
@@ -1,7 +1,23 @@
-let mainBlance = document.getElementById('main-amount').innerText;
-const donate = document.getElementById('btn-don');
-const history = document.getElementById('btn-his');
-let readyMoney = document.getElementById('ready-money');
-const inputAmount = document.getElementById('input-amount');
-console.log(readyMoney.innerText);
+const blance = document.getElementById('main-amount').innerText;
+const donate = document.getElementById('donate-click')
+const historyBtn = document.getElementById('btn-his');
+const readyMoney = document.getElementById('ready-money').innerText;
+// const inputBlance = document.getElementById('input-amount').value;
+const blogBtn = document.getElementById('blog-btn');
 
+
+donate.addEventListener('click',function(event){
+    event.preventDefault();
+    const inputBlance = document.getElementById('input-amount').value;
+    const mainBlance = parseFloat(blance);
+    const inputAmount = parseFloat(inputBlance);
+    const updateReadyMoney = parseFloat(readyMoney);
+    if(mainBlance < inputAmount || inputAmount <= 0){
+        alert('type correct amount');
+        return;
+    }
+    const newBlance = mainBlance - inputAmount;
+    document.getElementById('main-amount').innerText = newBlance;
+    const newReadyMoney = updateReadyMoney + inputAmount;
+    document.getElementById('ready-money').innerText = newReadyMoney;
+})
